00c07dc89ecb4506cd2557a555e94758722438f3
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-09-16 13:04:53 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -16,9 +16,21 @@ def example():
         {'name': 'мандарины', 'price': 95},
         {'name': 'манго', 'price': 321},
     ]
+    books= [
+        {'name': 'Шоколад', 'autor': 'Харрис Джоанн', 'janr': 'Роман', 'str':400},
+        {'name': 'Изгои', 'autor': 'С.Э.Хинтон', 'janr': 'Роман', 'str':388},
+        {'name': 'Звук и ярость', 'autor': 'У.Фолкнер', 'janr': 'Роман', 'str':416},
+        {'name': 'Книжный вор', 'autor': 'З.Маркус', 'janr': 'Роман', 'str':608},
+        {'name': 'Дом,в котором...', 'autor': 'М.Петросян', 'janr': 'Роман', 'str':960},
+        {'name': 'о мышах и людях', 'autor': 'Д.Стейнбек', 'janr': 'Повесть' , 'str':256},
+        {'name': 'С волками жить', 'autor': 'С.Райт', 'janr': 'Роман', 'str':519},
+        {'name': 'Жизненный план', 'autor': 'Лори Н. Спилман', 'janr': 'Детективный роман', 'str':352},
+        {'name': 'Великий Гетсби', 'autor': 'Ф.С.Фицджеральд', 'janr': 'Роман', 'str':224},
+        {'name': 'Одинокая звезда', 'autor': 'Д.Чиаверини', 'janr': 'Роман', 'str':352}
+    ]
     return render_template('example.html', 
                             name=name , lab_num=lab_num ,
-                            group=group , course=course, fruits=fruits )
+                            group=group , course=course, fruits=fruits, books=books )
 
 
 @app.route("/menu")
