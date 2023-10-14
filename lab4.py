2fbd4bb6ccd4586a3a9292c3ed1c83e92a1db7e4
Пушкарева Дарья <dasha.pushkareva.nsk@gmail.com>
2023-10-14 16:02:29 +0700

    холодил

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template, request
+from flask import Blueprint, render_template, request
 lab4=Blueprint('lab4', __name__)
 
 @lab4.route('/lab4/')
@@ -33,17 +33,29 @@ def order():
 
 
 
-@lab4.route('/lab4/xolod', methods = ['GET', 'POST'])
+@lab4.route('/lab4/xolod', methods = ['GET','POST'])
 def xolodil():
     errors={}
-    if request.method == 'GET':
-        return render_template('xolod.html',errors=errors)
-
-
     temp = request.form.get('temp')
-    print(temp)
     if temp == '':
-        errors['temp']= 'Не задана температура'
-    if temp <= '12':
-        errors['temp']= 'Не удалось установить температуру — слишком низкое значение»'
-    return render_template('xolod.html', temp=temp, errors=errors)
+       errors['temp'] = 'Не задана температура'
+    return render_template('xolod.html', errors=errors, temp=temp)
+
+
+@lab4.route('/lab4/temperatura', methods = ['GET', 'POST'])
+def temper():
+    errors={}
+    temp = request.form.get('temp')
+
+    if temp is not None and int(temp) <= '-12' :
+        errors['min']= 'Не удалось установить температуру — слишком низкое значение»'
+    if temp is not None and int(temp) >= '-1':
+        errors['max']= 'Не удалось установить температуру — слишком высокое значение'
+    if temp is not None and int(temp)>= '-12' and temp<= '-9' :
+        errors['sr']= f'Установлена температура {{int.(temp)}} ' 
+    if temp is not None and int(temp)>= '-8' and temp<= '-5' :
+        errors['men']= f'Установлена температура {{temp}}' 
+    if temp is not None and int(temp) >= '-4' and temp<= '-1' :
+        errors['bol']= f'Установлена температура {{temp}}'
+
+    return render_template('temper.html',temp=temp, errors=errors)
