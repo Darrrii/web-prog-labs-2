e3b40d891703e949314abd13cbbe4833c6a4c48d
Vladislav Demenev <v.demenev1905@gmail.com>
2023-10-28 11:57:44 +0700

    Вывод пользователей

---
+++ lab5.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, redirect
+import psycopg2
+
+lab5=Blueprint("lab5", __name__)
+
+@lab5.route("/lab5")
+def main():
+
+    conn= psycopg2.connect( host="127.0.0.1", database="knowledge_base", user="daria_knowledge_base", password="Daria123")
+
+    cur=conn.cursor()
+
+    cur.execute("select * from users;")
+
+    result=cur.fetchall()
+
+    cur.close()
+    conn.close()
+    print(result)
+
+
+
+    return "go to console"
+
+@lab5.route("/lab5/users")
+def use():
+
+    conn= psycopg2.connect( host="127.0.0.1", database="knowledge_base", user="daria_knowledge_base", password="Daria123")
+
+    cur=conn.cursor()
+
+    cur.execute("select * from users;")
+
+    result=cur.fetchall()
+
+    cur.close()
+    conn.close()
+    print(result)
+
+
+
+    return "go to console"
