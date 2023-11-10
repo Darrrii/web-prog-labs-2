a06a0cfd72ec899aeab88df6ad15637816196522
Пушкарева Дарья <dasha.pushkareva.nsk@gmail.com>
2023-11-10 23:32:36 +0700

    Лаба 5

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 
+
+app = Flask(__name__)
+app.secret_key = "123"
+
 app= Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -12,18 +16,5 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 
-@app.route('/lab2/zadanie')
-def zadanie():
-    A,B,C,N,K,G,H,summa = 1,2,3,10,6,2,3,0
-    if A<B<C:
-        A=A*2
-        B=B*2
-        C=C*2
-        result= str(N)*K
-
-        for i in range(1, H+1):
-            summa += i**G
-        slov= [{'A': A, 'B':B, 'C': C, 'summa': summa, 'result': result}]
-        return render_template('zadanie.html',slov=slov)
 
 
