2e328e368145cc87c665247bb38417a024f3dff8
Vladislav Demenev <v.demenev1905@gmail.com>
2023-11-25 10:35:37 +0700

    Заготовка к 7 лабораторной

--- lab1.py
+++ lab1.py
@@ -28,6 +28,8 @@ def menu():
         <a href="/lab3/" target="_blank">Лабораторная работа 3</a>
         <a href="/lab4/" target="_blank">Лабораторная работа 4</a>
         <a href="/lab5" target="_blank">Лабораторная работа 5</a>
+        <a href="/lab6" target="_blank">Лабораторная работа 6</a>
+        <a href="/lab7" target="_blank">Лабораторная работа 7</a>
         </menu>
         <footer>
             &copy; Пушкарева Дарья, Андронова Софья, ФБИ-12, 3 курс, 2023
