6b7a57a4a8f15079446938738f10d74a3b1a5c22
Пушкарева Дарья <dasha.pushkareva.nsk@gmail.com>
2023-12-22 14:39:41 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -5,3 +5,14 @@ lab9= Blueprint('lab9', __name__)
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return "нет такой страницы",404
+
+@lab9.route('/lab9/404')
+def error_404():
+    return render_template('lab9/error404.html')
+
+if __name__ == '__main__':
+    lab9.run()
